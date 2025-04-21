a9f43d35d2bf5588aff0b9d68ff70ebcb9e9c328
BeanSamuel <samuelchao921221@gmail.com>
2025-04-22 04:25:54 +0800

    CSES Dice_Combinations

---
+++ CSES/Dynamic_Programming/Dice_Combinations.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define F first
+#define S second
+#define EB emplace_back	
+#define PB push_back
+#define siz(v) ((int)v.size())  
+#define ALL(x) x.begin(), x.end()
+
+template<typename T> using vec = vector<T>;
+template <class T> bool chmin(T &a, T b) { return (b < a and (a = b, true)); }
+template <class T> bool chmax(T &a, T b) { return (a < b and (a = b, true)); }
+template <class T> inline constexpr T inf = numeric_limits<T>::max() / 2;
+
+const int MOD =  1000000007;
+const double PI = 3.14159265358;
+const double EPS = 1e-9;
+const int xx[8] = {0,1,0,-1,1,1,-1,-1}; const int yy[8] = {1,0,-1,0,1,-1,-1,1};
+
+void pmod(ll &a, ll b) {a = (a+b)%MOD;}
+void mmod(ll &a, ll b) {a = (a-b+MOD)%MOD;} 
+void tmod(ll &a, ll b) {a = (a*b)%MOD;}
+ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a%b);}
+ll POW(ll a, ll b) {ll res=1; do{if(b%2)tmod(res,a);tmod(a,a);}while(b>>=1); return res;}
+
+const int MXN = 1e8; const int N = MXN + 10;
+#define pii pair<int,int>
+
+using namespace std;
+
+void solve(){
+    int n; cin>>n;
+    vec<ll> dp(n+1,0);
+    dp[1] = dp[2] = dp[3] = dp[4] = dp[5] = dp[6] = 1;
+    for(int i=1;i<=n;i++) {
+        if(i>0) dp[i] = (dp[i] + dp[i-1]) % MOD;
+        if(i>1) dp[i] = (dp[i] + dp[i-2]) % MOD; 
+        if(i>2) dp[i] = (dp[i] + dp[i-3]) % MOD; 
+        if(i>3) dp[i] = (dp[i] + dp[i-4]) % MOD; 
+        if(i>4) dp[i] = (dp[i] + dp[i-5]) % MOD; 
+        if(i>5) dp[i] = (dp[i] + dp[i-6]) % MOD;
+    }
+    cout<<dp[n]%MOD<<endl;
+}
+
+
+signed main(){
+    fastio;
+	int t=1; //cin>>t;
+	while(t--) solve();
+}
