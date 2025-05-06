d60bfab218bc682427574b1a8db61b3e46bea893
BeanSamuel <samuelchao921221@gmail.com>
2025-05-06 19:18:42 +0800

    leetcode 1920

---
+++ leetcode/1920_Build_Array_from_Permutation.cpp
@@ -0,0 +1,29 @@
+/*
+Question: 
+Link: 
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx2,tune=native")
+
+template <class T> bool chmax(T &a, T b) { return (a < b and (a = b, true)); }
+template <class T> bool chmin(T &a, T b) { return (b < a and (a = b, true)); }
+
+static int x = []() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    return 0;
+}();
+
+class Solution {
+    public:
+        vector<int> buildArray(vector<int>& nums) {
+            vector<int> ans(nums.size());
+            for(int i=0;i<nums.size();i++) {
+                ans[i] = nums[nums[i]];
+            }
+            return ans;
+        }
+    };
