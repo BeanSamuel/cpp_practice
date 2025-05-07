8d158f7ee8eb072d1306e484c72b300dee6da7fd
BeanSamuel <samuelchao921221@gmail.com>
2025-05-07 21:24:32 +0800

    leetcode 3341

---
+++ leetcode/3341_Find_Minimum_Time_to_Reach_Last_Room_I.cpp
@@ -0,0 +1,47 @@
+/*
+Question: 
+Link: 
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx2,tune=native")
+
+template <class T> bool chmax(T &a, T b) { return (a < b and (a = b, true)); }
+template <class T> bool chmin(T &a, T b) { return (b < a and (a = b, true)); }
+
+static int x = []() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    return 0;
+}();
+
+class Solution {
+    public:
+        int minTimeToReach(vector<vector<int>>& moveTime) {
+            int n = moveTime.size(), m = moveTime[0].size();
+            vector<vector<int>> v(n, vector<int>(m, INT_MAX)); 
+            priority_queue<pair<int, pair<int,int>>> pq;
+            v[0][0] = 0;
+            pq.push({0,{0,0}});
+            while(!pq.empty()) {
+                auto [d,pos] = pq.top();
+                pq.pop();
+                if( d > v[pos.first][pos.second]) continue;
+                const int xx[4] = {0,1,0,-1}; const int yy[4] = {1,0,-1,0};
+                for(int i=0;i<4;i++) {
+                    int nextx = pos.first+xx[i], nexty = pos.second+yy[i];
+                    if((0<=nextx&&nextx<n)&&(0<=nexty&&nexty<m)){
+                        int tmp = v[pos.first][pos.second] + max(1, moveTime[nextx][nexty] - v[pos.first][pos.second]+1);
+                        if(tmp < v[nextx][nexty]){
+                            v[nextx][nexty] = tmp;
+                            pq.push({tmp, {nextx,nexty}});
+                        }
+                    }
+                }
+            }
+            
+            return v[n-1][m-1];
+        }
+    };
