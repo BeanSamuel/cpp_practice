7f42f11a0e7d65ba99d7af49e3433fcb25db5805
BeanSamuel <samuelchao921221@gmail.com>
2025-07-13 20:38:13 +0800

    codeforces LI2_Contests Contest_17_Set_and_Map C

---
+++ codeforces/LI2_Contests/Contest_17_Set_and_Map/C.cpp
@@ -0,0 +1,59 @@
+/*
+Question: C. Minimum Index
+Link: https://codeforces.com/group/jtU6D2hVEi/contest/533251/problem/C
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define F first
+#define S second
+#define EB emplace_back	
+#define PB push_back
+#define siz(v) ((int)v.size())  
+#define ALL(x) x.begin(), x.end()
+
+template<typename T> using vec = vector<T>;
+template <class T> bool chmin(T &a, T b) { return (b < a and (a = b, true)); }
+template <class T> bool chmax(T &a, T b) { return (a < b and (a = b, true)); }
+template <class T> inline constexpr T inf = numeric_limits<T>::max() / 2;
+
+const int MOD =  998244353;
+const double PI = 3.14159265358;
+const double EPS = 1e-9;
+const int xx[8] = {0,1,0,-1,1,1,-1,-1}; const int yy[8] = {1,0,-1,0,1,-1,-1,1};
+
+void pmod(ll &a, ll b) {a = (a+b)%MOD;}
+void mmod(ll &a, ll b) {a = (a-b+MOD)%MOD;} 
+void tmod(ll &a, ll b) {a = (a*b)%MOD;}
+ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a%b);}
+ll POW(ll a, ll b) {ll res=1; do{if(b%2)tmod(res,a);tmod(a,a);}while(b>>=1); return res;}
+
+const int MXN = 1e8; const int N = MXN + 10;
+#define pii pair<int,int>
+
+void solve(){
+    ll b, x, y, c, d, m; cin>>b>>x>>y>>c>>d>>m;
+    ll a = (b*x+y)%m;
+    map<ll,int> mp;
+    mp[a] = 0;
+    for(int i=1;i<=1000000;i++) {
+        x = (c*a+x)%m;
+        y = (d*a+y)%m;
+        a = (b*x+y)%m;
+        if(mp.find(a)!=mp.end()) {
+            cout<<i<<" "<<i-mp[a]<<endl;
+            return;
+        } else {
+            mp[a] = i;
+        }
+    }
+    cout<<"GREATER"<<endl;
+}
+
+signed main(){
+    fastio;
+	int t=1; //cin>>t;
+	while(t--) solve();
+}
