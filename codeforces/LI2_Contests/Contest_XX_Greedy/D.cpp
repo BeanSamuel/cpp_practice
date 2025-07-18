1404a1032837ceeda90114299be6401e6a1d8a5b
BeanSamuel <samuelchao921221@gmail.com>
2025-07-18 20:42:48 +0800

    codeforces LI2_Contests Contest_XX_Greedy D

---
+++ codeforces/LI2_Contests/Contest_XX_Greedy/D.cpp
@@ -0,0 +1,50 @@
+/*
+Question: D. Magic Spell
+Link: https://codeforces.com/group/jtU6D2hVEi/contest/533371/problem/D
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define F first
+#define S second
+#define EB emplace_back	
+#define PB push_back
+#define siz(v) ((int)v.size())  
+#define ALL(x) x.begin(), x.end()
+ 
+template<typename T> using vec = vector<T>;
+template <class T> bool chmin(T &a, T b) { return (b < a and (a = b, true)); }
+template <class T> bool chmax(T &a, T b) { return (a < b and (a = b, true)); }
+template <class T> inline constexpr T inf = numeric_limits<T>::max() / 2;
+ 
+const int MOD =  998244353;
+const double PI = 3.14159265358;
+const double EPS = 1e-9;
+const int xx[8] = {0,1,0,-1,1,1,-1,-1}; const int yy[8] = {1,0,-1,0,1,-1,-1,1};
+ 
+void pmod(ll &a, ll b) {a = (a+b)%MOD;}
+void mmod(ll &a, ll b) {a = (a-b+MOD)%MOD;} 
+void tmod(ll &a, ll b) {a = (a*b)%MOD;}
+ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a%b);}
+ll POW(ll a, ll b) {ll res=1; do{if(b%2)tmod(res,a);tmod(a,a);}while(b>>=1); return res;}
+ 
+const int MXN = 1e8; const int N = MXN + 10;
+#define pii pair<int,int>
+ 
+void solve(){
+    string s; cin>>s;
+    string s2; cin>>s2;
+    sort(ALL(s2),greater<char>());
+    for(int i=0,j=0;i<s.length()&&j<s2.length();i++) {
+        if(s2[j]>s[i]) s[i] = s2[j++];
+    }
+    cout<<s<<endl;
+}
+ 
+signed main(){
+    fastio;
+	int t=1; //cin>>t;
+	while(t--) solve();
+}
