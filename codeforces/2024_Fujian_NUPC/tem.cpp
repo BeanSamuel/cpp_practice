90ccdffa0da0d756c370c081562a996ea88143bb
BeanSamuel <samuelchao921221@gmail.com>
2025-08-08 16:58:34 +0800

    Co-authored-by: Hippotumux <Hippotumux@users.noreply.github.com> Co-authored-by: WU YU <wuyu06@users.noreply.github.com>
    codeforces 2024_Fujian_NUPC tem

---
+++ codeforces/2024_Fujian_NUPC/tem.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define F first
+#define S second
+#define siz(v) ((int)v.size())
+#define ALL(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define EB emplace_back
+#define PB push_back
+#define endl '\n'
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+template<typename T> using vec = vector<T>;
+template<typename T> using Prior = priority_queue<T>;
+template<typename T> using prior = priority_queue<T, vec<T>, greater<T> >;
+template <class T> bool chmin(T &a, T b) { return (b< a and (a = b,true));}
+template <class T> bool chmax(T &a, T b) { return (a < b and (a = b,true));}
+template <class T> inline constexpr T inf = numeric_limits<T>::max()/2;
+#ifdef LOCAL
+template<class... T> void dbg(T... x){
+    char e{};
+    ((cerr<< e << x , e = ' '),...);
+}
+#define debug(x...) dbg(#x, '=',x,'\n')
+#else
+#define debug(...)((void)0)
+#endif
+
+void solve(){
+}
+signed main(void) {
+    ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+    ll t =1;
+    while(t--) solve();
+}
+
