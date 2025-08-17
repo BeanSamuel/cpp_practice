dc6677af5507e587bbf8aeafef46da720da27f15
BeanSamuel <samuelchao921221@gmail.com>
2025-08-17 13:45:13 +0800

    update 2017_3 C

---
+++ APCS/2017_3/C.cpp
@@ -0,0 +1,62 @@
+/*
+Question: 小群體
+Link: https://zerojudge.tw/ShowProblem?problemid=c291
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define F first
+#define S second
+#define EB emplace_back	
+#define PB push_back
+#define siz(v) ((int)v.size())  
+#define ALL(x) x.begin(), x.end()
+
+template<typename T> using vec = vector<T>;
+template <class T> bool chmin(T &a, T b) { return (b < a and (a = b, true)); }
+template <class T> bool chmax(T &a, T b) { return (a < b and (a = b, true)); }
+template <class T> inline constexpr T inf = numeric_limits<T>::max() / 2;
+
+const int MOD =  998244353;
+const double PI = 3.14159265358;
+const double EPS = 1e-9;
+const int xx[8] = {0,1,0,-1,1,1,-1,-1}; const int yy[8] = {1,0,-1,0,1,-1,-1,1};
+
+void pmod(ll &a, ll b) {a = (a+b)%MOD;}
+void mmod(ll &a, ll b) {a = (a-b+MOD)%MOD;} 
+void tmod(ll &a, ll b) {a = (a*b)%MOD;}
+ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a%b);}
+ll POW(ll a, ll b) {ll res=1; do{if(b%2)tmod(res,a);tmod(a,a);}while(b>>=1); return res;}
+const int MXN = 1e8; const int N = MXN + 10;
+#define pii pair<int,int>
+
+void solve(){
+    int n, dir; cin>>n>>dir;
+    int arr[n][n];
+    for(int i=0;i<n;i++) for(int j=0;j<n;j++) cin>>arr[i][j];
+    pii now = {n/2, n/2};
+    int move = 1;
+    cout<<arr[now.F][now.S];
+    for(int i=1;i<=n+(n&~1);i++) {
+        for(int j=0;j<move;j++) {
+            if(dir == 0) now.S--;
+            else if(dir == 1) now.F--;
+            else if(dir == 2) now.S++;
+            else if(dir == 3) now.F++;
+            cout<<arr[now.F][now.S];
+        }
+        if((i!=0)&&(i%2==0)) move++;
+		if(i==n+(n&~1)-1) move=n-1;
+		dir = (dir+1)%4;
+    }
+    cout<<endl;
+}
+
+signed main(){
+    fastio;
+	int t=1; //cin>>t;
+	while(t--) solve();
+}
