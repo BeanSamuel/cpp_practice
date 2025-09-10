f0dc49c46cc9c6d97e767b5140ecabd2a3d22214
BeanSamuel <samuelchao921221@gmail.com>
2025-09-10 20:05:11 +0800

    CSES Sum_of_Two_Values

---
+++ CSES/Sorting_and_Searching/Sum_of_Two_Values.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define F first
+#define S second
+#define EB emplace_back	
+#define PB push_back
+#define siz(v) ((int)v.size())  
+#define ALL(x) x.begin(), x.end()
+
+template<typename T> using vec = vector<T>;
+template <class T> bool chmin(T &a, T b) { return (b < a and (a = b, true)); }
+template <class T> bool chmax(T &a, T b) { return (a < b and (a = b, true)); }
+template <class T> inline constexpr T inf = numeric_limits<T>::max() / 2;
+
+const int MOD =  998244353;
+const double PI = 3.14159265358;
+const double EPS = 1e-9;
+const int xx[8] = {0,1,0,-1,1,1,-1,-1}; const int yy[8] = {1,0,-1,0,1,-1,-1,1};
+
+void pmod(ll &a, ll b) {a = (a+b)%MOD;}
+void mmod(ll &a, ll b) {a = (a-b+MOD)%MOD;} 
+void tmod(ll &a, ll b) {a = (a*b)%MOD;}
+ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a%b);}
+ll POW(ll a, ll b) {ll res=1; do{if(b%2)tmod(res,a);tmod(a,a);}while(b>>=1); return res;}
+
+const int MXN = 1e8; const int N = MXN + 10;
+#define pii pair<int,int>
+
+void solve(){
+    int n, x; cin>>n>>x;
+    map<int, vec<int>> mp;
+    for(int i=1;i<=n;i++) {
+        int a; cin>>a;
+        mp[a].EB(i);
+    }
+    for(auto i:mp) {
+        int a = x - i.F;
+        if(a*2 == x && siz(mp[a])>=2) {
+            cout<<mp[a][0]<<" "<<mp[a][1]<<endl;
+            return;
+        } else if(a*2 != x && mp.find(a)!=mp.end()){
+            cout<<mp[i.F][0]<<" "<<mp[a][0]<<endl;
+            return;
+        }
+    }
+    cout<<"IMPOSSIBLE"<<endl;
+}
+
+
+signed main(){
+    fastio;
+	int t=1; //cin>>t;
+	while(t--) solve();
+}
