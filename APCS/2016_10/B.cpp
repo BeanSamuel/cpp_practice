e3dab240ae181ade3e3d62bacf513cc2f2e23b4b
BeanSamuel <samuelchao921221@gmail.com>
2025-09-18 20:49:49 +0800

    APCS 2016_10 B

---
+++ APCS/2016_10/B.cpp
@@ -0,0 +1,63 @@
+/*
+Question: 最大和
+Link: https://zerojudge.tw/ShowProblem?problemid=c295
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define F first
+#define S second
+#define EB emplace_back	
+#define PB push_back
+#define siz(v) ((int)v.size())  
+#define ALL(x) x.begin(), x.end()
+
+template<typename T> using vec = vector<T>;
+template <class T> bool chmin(T &a, T b) { return (b < a and (a = b, true)); }
+template <class T> bool chmax(T &a, T b) { return (a < b and (a = b, true)); }
+template <class T> inline constexpr T inf = numeric_limits<T>::max() / 2;
+
+const int MOD =  998244353;
+const double PI = 3.14159265358;
+const double EPS = 1e-9;
+const int xx[8] = {0,1,0,-1,1,1,-1,-1}; const int yy[8] = {1,0,-1,0,1,-1,-1,1};
+
+void pmod(ll &a, ll b) {a = (a+b)%MOD;}
+void mmod(ll &a, ll b) {a = (a-b+MOD)%MOD;} 
+void tmod(ll &a, ll b) {a = (a*b)%MOD;}
+ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a%b);}
+ll POW(ll a, ll b) {ll res=1; do{if(b%2)tmod(res,a);tmod(a,a);}while(b>>=1); return res;}
+const int MXN = 1e8; const int N = MXN + 10;
+#define pii pair<int,int>
+
+void solve(){
+	int n, m; cin>>n>>m;
+	vec<int> v(n);
+	for(auto &i: v){
+		for(int j=0;j<m;j++) {
+			int x; cin>>x;
+			chmax(i, x);
+		}
+	}
+	int sum = accumulate(ALL(v), 0);
+	cout<<sum<<endl;
+	vec<int> ans;
+	for(auto i:v) if(sum%i==0) ans.EB(i);
+	if(siz(ans)==0) cout<<-1<<endl;
+	else {
+		for(int i=0;i<siz(ans);i++) {
+			cout<<ans[i]<<" \n"[i==siz(ans)-1];
+		}
+	}
+
+}
+
+
+signed main(){
+    fastio;
+	int t=1; //cin>>t;
+	while(t--) solve();
+}
