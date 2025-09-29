2c12a8e290fdfb6f7a761f2160cdeae7b28fdff0
BeanSamuel <samuelchao921221@gmail.com>
2025-09-30 01:02:18 +0800

    APCS 2017_10 C

---
+++ APCS/2017_10/C.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define F first
+#define S second
+#define EB emplace_back	
+#define PB push_back
+#define siz(v) ((int)v.size())  
+#define ALL(x) x.begin(), x.end()
+#define rall(x) x.rbegin(),x.rend()
+
+template<typename T> using vec = vector<T>;
+template <class T> bool chmin(T &a, T b) { return (b < a and (a = b, true)); }
+template <class T> bool chmax(T &a, T b) { return (a < b and (a = b, true)); }
+template <class T> inline constexpr T inf = numeric_limits<T>::max() / 2;
+
+const int MOD =  998244353;
+const double PI = 3.14159265358;
+const double EPS = 1e-9;
+const int xx[8] = {0,1,0,-1,1,1,-1,-1}; const int yy[8] = {1,0,-1,0,1,-1,-1,1};
+
+void pmod(ll &a, ll b) {a = (a+b)%MOD;}
+void mmod(ll &a, ll b) {a = (a-b+MOD)%MOD;} 
+void tmod(ll &a, ll b) {a = (a*b)%MOD;}
+ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a%b);}
+ll POW(ll a, ll b) {ll res=1; do{if(b%2)tmod(res,a);tmod(a,a);}while(b>>=1); return res;}
+
+const int MXN = 1e8; const int N = MXN + 10;
+#define pii pair<int,int>
+#define pll pair<ll, ll>
+
+#ifdef LOCAL
+template<class... T> void dbg(T...x) {
+    char e{};
+    ((cerr << e << x, e = ' '),...);
+}
+#define debug(x...) dbg(#x, '=',x,'\n')
+#else 
+#define debug(...) ((void)0)
+#endif
+
+void solve(){
+    int n; cin>>n;
+    vec<bool> chk_root(100005, false);
+    vec<int> g[100005];
+    for(int i=1;i<=n;i++) {
+        int t; cin>>t;
+        while(t--) {
+            int v; cin>>v;
+            chk_root[v] = 1;
+            g[i].EB(v);
+        }
+    }
+    int root = 0;
+    for(int i=1;i<=n;i++) if(!chk_root[i]) root=i;
+    
+    ll ans = 0;
+
+    auto dfs = [&](auto self, int now) -> ll {
+        ll ret = 0;
+        for(auto i: g[now]) {
+            chmax(ret, self(self, i));
+        }
+        ans += ret;
+        return ret+1;
+    };
+
+    dfs(dfs, root);
+    cout<<root<<endl;
+    cout<<ans<<endl;
+}
+
+
+
+signed main(){
+    fastio;
+	int t=1; //cin>>t;
+	while(t--) solve();
+}
