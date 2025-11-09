3781a71a19ae42a3ff3134441512ba226575470c
BeanSamuel <samuelchao921221@gmail.com>
2025-11-09 13:18:59 +0800

    CSES Sliding_Window_Xor

---
+++ CSES/Sliding_Window_Problems/Sliding_Window_Xor.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define F first
+#define S second
+#define EB emplace_back	
+#define PB push_back
+#define siz(v) ((int)v.size())  
+#define ALL(x) x.begin(), x.end()
+#define rall(x) x.rbegin(),x.rend()
+
+template<typename T> using vec = vector<T>;
+template <class T> bool chmin(T &a, T b) { return (b < a and (a = b, true)); }
+template <class T> bool chmax(T &a, T b) { return (a < b and (a = b, true)); }
+template <class T> inline constexpr T inf = numeric_limits<T>::max() / 2;
+
+ll MOD =  998244353;
+const double PI = 3.14159265358;
+const double EPS = 1e-9;
+const int xx[8] = {0,1,0,-1,1,1,-1,-1}; const int yy[8] = {1,0,-1,0,1,-1,-1,1};
+
+void pmod(ll &a, ll b) {a = (a+b)%MOD;}
+void mmod(ll &a, ll b) {a = (a-b+MOD)%MOD;} 
+void tmod(ll &a, ll b) {a = (a*b)%MOD;}
+ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a%b);}
+ll POW(ll a, ll b) {ll res=1; do{if(b%2)tmod(res,a);tmod(a,a);}while(b>>=1); return res;}
+
+const int MXN = 1e8; const int N = MXN + 10;
+#define pii pair<int,int>
+#define pll pair<ll, ll>
+
+void solve(){
+    ll n, k; cin>>n>>k;
+    ll x, a, b, c; cin>>x>>a>>b>>c;
+    ll pre = x, cnt = x, left = x, now = x;
+    for(auto i=1;i<k;i++) {
+        now = (a*pre + b)%c;
+        cnt ^= now;
+        pre = now;
+    }
+    ll ans = cnt;
+    for(auto i=k;i<n;i++) {
+        cnt ^= left;
+        now = (a*pre + b)%c;
+        left = (a*left + b)%c;
+        cnt ^= now;
+        pre = now;
+        ans ^= cnt;
+    }
+    cout<<ans<<endl;
+}
+
+signed main(){
+    fastio;
+	int t=1; //cin>>t;
+	while(t--) solve();
+}
