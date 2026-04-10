77023081e347cc5af3fd1ea76e9319cef32c7139
BeanSamuel <samuelchao921221@gmail.com>
2026-04-10 13:26:31 +0800

    CSES Dynamic_Programming Counting_Towers

---
+++ CSES/Dynamic_Programming/Counting_Towers.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define F first
+#define S second
+#define EB emplace_back	
+#define PB push_back
+#define siz(v) ((int)v.size())  
+#define ALL(x) x.begin(), x.end()
+
+template<typename T> using vec = vector<T>;
+template <class T> bool chmin(T &a, T b) { return (b < a and (a = b, true)); }
+template <class T> bool chmax(T &a, T b) { return (a < b and (a = b, true)); }
+template <class T> inline constexpr T inf = numeric_limits<T>::max() / 2;
+
+const int MOD =  1000000007;
+const double PI = 3.14159265358;
+const double EPS = 1e-9;
+const int xx[8] = {0,1,0,-1,1,1,-1,-1}; const int yy[8] = {1,0,-1,0,1,-1,-1,1};
+
+void pmod(ll &a, ll b) {a = (a+b)%MOD;}
+void mmod(ll &a, ll b) {a = (a-b+MOD)%MOD;} 
+void tmod(ll &a, ll b) {a = (a*b)%MOD;}
+ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a%b);}
+ll POW(ll a, ll b) {ll res=1; do{if(b%2)tmod(res,a);tmod(a,a);}while(b>>=1); return res;}
+
+const int MXN = 1e8; const int N = MXN + 10;
+#define pii pair<int,int>
+
+ll dp[1000005][2];
+
+void solve(){
+    dp[1][0] = 1;
+    dp[1][1] = 1;
+    for(int i=2;i<1000001;i++) {
+        dp[i][0] = (2*dp[i-1][0] + dp[i-1][1])%MOD;
+        dp[i][1] = (dp[i-1][0] + 4*dp[i-1][1])%MOD;
+    }
+}
+
+
+signed main(){
+    fastio;
+    solve();
+	int t=1; cin>>t;
+	while(t--) {
+        int x; cin>>x;
+        cout<<(dp[x][0]+dp[x][1])%MOD<<endl;
+    };
+}
